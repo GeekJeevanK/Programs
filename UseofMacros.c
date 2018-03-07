efd7f37d476c0dbf0556cd5c8bc4163fa25258b8
GeekJeevanK <37112903+GeekJeevanK@users.noreply.github.com>
2018-03-07 20:54:29 +0530

    Macros

---
+++ UseofMacros.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#define MUL(a,b) a*b
+int main()
+{
+    printf("%d",MUL(10,20));
+
+    return 0;
+}
+
+
+
